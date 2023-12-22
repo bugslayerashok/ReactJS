e43ff502a2d7dee80e3efa205d55cec2b87f3e52
AshokPrajapati263 <prajapatiashok263@gmail.com>
2023-12-22 14:15:15 +0530

    I have explain how react structure works

---
+++ firstpro-vite/src/Demo.jsx
@@ -0,0 +1,7 @@
+function Demo()
+{
+    return (
+        <><h1>this is the demo</h1></>
+    );
+}
+export default Demo;
--- firstpro/src/index.js
+++ firstpro/src/index.js
@@ -1,28 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
 
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); */
-const myelement = <h1>I {5+5} Love JSX!</h1>;
-const ele = React.createElement("h1",{},"Hello in h1");
-const bigcode = (
-  <><h1>Hello</h1>
-  
-  <p>lo</p>
-  </>
-);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(bigcode);
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

816691e2f40455160a2978e066d2edda6690f406
AshokPrajapati263 <prajapatiashok263@gmail.com>
2023-12-25 14:02:15 +0530

    01Counter Project Created..

---
+++ 01Counter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(0);
+  const addCounter = () => {
+    setCounter(counter++);
+  };
+  const removeCounter = () => {
+    setCounter(counter--);
+  };
+  return (
+    <>
+      <h1>Hii First Project</h1>
+      <h2>counter : {counter}</h2>
+      <button onClick={addCounter}>Add : {counter}</button>
+      &nbsp;&nbsp;&nbsp;<button onClick={removeCounter}>Remove (Dec) : {counter}</button>
+    </>
+  )
+}
+
+export default App

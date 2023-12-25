1e7f824350ed7bebe037ade863324092e78b397e
AshokPrajapati263 <prajapatiashok263@gmail.com>
2023-12-25 14:19:45 +0530

    modified

--- 01Counter/src/App.jsx
+++ 01Counter/src/App.jsx
@@ -4,15 +4,15 @@ import './App.css'
 function App() {
   let [counter,setCounter] = useState(0);
   const addCounter = () => {
-    setCounter(counter++);
+    setCounter(++counter);
   };
   const removeCounter = () => {
-    setCounter(counter--);
+    setCounter(--counter);
   };
   return (
     <>
       <h1>Hii First Project</h1>
-      <h2>counter : {counter}</h2>
+      <h2>Counter : {counter}</h2>
       <button onClick={addCounter}>Add : {counter}</button>
       &nbsp;&nbsp;&nbsp;<button onClick={removeCounter}>Remove (Dec) : {counter}</button>
     </>

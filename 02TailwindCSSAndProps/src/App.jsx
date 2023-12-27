cf2b762b348601d1d1f5e8c41a1bc7ff9c1f307c
AshokPrajapati263 <prajapatiashok263@gmail.com>
2023-12-27 14:25:07 +0530

    02 tailwind project with card inserted.

---
+++ 02TailwindCSSAndProps/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CardTwo from './CardTwo'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline m-6">
+      Hello world!
+    </h1>
+    <CardTwo/>
+    </>
+  )
+}
+
+export default App

ec43b85ae9862907576f72c0ab463ed00e34267b
AshokPrajapati263 <prajapatiashok263@gmail.com>
2023-12-28 14:42:26 +0530

    Props Examples

--- 02TailwindCSSAndProps/src/App.jsx
+++ 02TailwindCSSAndProps/src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import CardTwo from './CardTwo'
 
 function App() {
-
+  let obj = {
+    username : "Hiii"
+  }
   return (
     <>
       <h1 className="text-3xl font-bold underline m-6">
       Hello world!
     </h1>
-    <CardTwo/>
+    <div className="cols-3 gap-3 justify-self-center">
+    <CardTwo mainTitle="Ashok" btnText="Click Me!" objVal={obj}/>
+    <CardTwo mainTitle="Rahul" btnText="Click Me!"/>
+    <CardTwo mainTitle="OM" btnText="Click Me!"/>
+    </div>
+    
     </>
   )
 }

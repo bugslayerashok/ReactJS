8ff17eb12f617b828829a17abbd8ffeb3fcd7e37
AshokPrajapati263 <prajapatiashok263@gmail.com>
2024-01-03 14:21:00 +0530

    04useCallbackAnduseMemo

---
+++ 04useCallbackAnduseMemo/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState , useCallback } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Components/Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default App;

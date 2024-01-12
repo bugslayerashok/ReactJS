1837f2e6a7a27466b6e2bbb4b972cfdbb8bff384
AshokPrajapati263 <prajapatiashok263@gmail.com>
2024-01-12 14:39:16 +0530

    TodoList

---
+++ firstpro/src/TodoList.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+            <button onClick={() => toggleComplete(index)}>Complete</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button onClick={addTodo} disabled={!newTodo.trim()}>
+          Add Todo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
--- firstpro/src/index.js
+++ firstpro/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import MemoEx from "./MemoEx";
+import TodoList from "./TodoList";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <>
-        <MemoEx />
+        <TodoList/>
     </>
 );
